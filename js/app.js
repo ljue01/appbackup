1228d3b2b1be957654aa9636c93e99c53217c810
ljue01 <liujueyi@liujueyi.cn>
2024-03-05 15:51:22 +0800

    跑通数据

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 // by Just. Liu
 // v0.0.1
 
+
 const app = Vue.createApp({	
   data() {
     return {
-
+			 items: [],
+			 selectedType: 0,
+			 show: false,
     }
   },
   mounted() {
@@ -15,12 +18,31 @@ const app = Vue.createApp({
 		});
 	},
 	computed: {
-		
+		// filteredItems() {
+		// 	return this.selectedType === '0'
+		// 		? this.items
+		// 		: this.items.filter(item => item.classid === parseInt(this.selectedType))
+		// },
+		filteredItems() {
+			let filtered = this.selectedType === ''
+				? this.items
+				: this.items.filter(item => item.classid === parseInt(this.selectedType));
+			// 反序排列
+			return filtered.slice().reverse();
+		},
 	},
   methods: {
-		
+		// 显示弹层
+		showPopup(item){
+			this.popupItem = item;
+			this.show = true;
+		},
+		// 隐藏弹层
+		hidePopup(){
+			this.popupItem = null;
+			this.show = false;
+		},
 		// 回到顶部
-		
   }
 })
 
